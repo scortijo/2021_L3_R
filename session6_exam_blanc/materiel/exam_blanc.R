74851c1b1f28f0ee5d0ff2e80984dd899c5ef756
OceaneCsn <oceane.cssn@gmail.com>
2021-12-02 12:00:23 +0100

    added exam blanc

---
+++ session6_exam_blanc/materiel/exam_blanc.R
@@ -0,0 +1,89 @@
+# Exam blanc, 2 décembre 2021 8h-9h30
+
+
+### Préparation de l'environnement de travail
+
+## Question 0
+
+# Chargez les librairies dont vous aurez besoin. 
+
+
+# Réglez votre répertoire de travail
+
+
+# Chargez le fichier _data_expression_cortijo2017.txt_ (qui est dans le dossier /data) 
+# dans R et sauvez le dans un objet
+
+
+
+### Vérification des données
+
+## Question 1
+
+# Est-ce que le tableau importé dans R contient le bon nombre de gènes?
+
+
+
+
+# Est-ce que le tableau importé dans R contient les données d'expression dans 
+# les 6 conditions (15 minutes à 17°C, 1 heure à 17°C, 4 heures à 17°C, 
+# 15 minutes à 27°C, 1 heure à 27°C et 4 heures à 27°C) ainsi que 
+# l'information pour le cluster d'appartenance de chaque gène? 
+# (Expliquez quelle commande vous permet de répondre)
+
+
+
+
+## Question 2
+
+# Est-ce que le tableau importé dans R contient bien le nombre de clusters attendus?
+# Quels sont leurs noms?
+  
+
+  
+## Question 3
+  
+# Faites un graphique montrant l'évolution au cours du temps de l'expression 
+# de ces deux gènes à 17°C et 27°C (AT1G04520 et AT3G12580). 
+# Obtenez vous des comportement d'expression correspondants aux 
+# résultats préliminaires obtenus sur ces deux gènes?
+
+
+
+
+## Question 4
+
+# Combien de gènes y a-t-il dans chaque cluster?
+  
+  
+
+## Question 5
+
+# Chargez la table _gene_fonction_cortijo2017.txt_ dans R et sauvez la dans un objet.
+# Puis combinez la avec la table contenant l'expression des gènes 
+# ( _data_expression_cortijo2017.txt_ chargée en début d'examen) 
+# de manière à rajouter l'information sur la fonction des gènes à la table 
+# contenant l'expression des gènes et le cluster d'appartenance de chaque gène
+
+
+
+# A partir de la nouvelle table combinée, trouvez le nom de AT3G12580. 
+# Quelle fonction ce nom suggère t-il pour AT3G12580 en réponse à la chaleur?
+
+
+
+# Filtrez la table combinée pour ne garder que le cluster 6A et déterminez 
+# combien de gènes de ce cluster répondent à la chaleur. 
+
+
+
+# Faites la même chose pour la table contenant tous les gènes. 
+# Combien de gènes dans la table complète répondent à la chaleur? 
+
+
+
+# Qu'est ce que le nombre de gènes répondant à la chaleur dans le 
+# cluster 6A par rapport à la table entière indique-t-il?
+
+
+  
